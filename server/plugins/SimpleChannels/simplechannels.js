e6c46a9d1d5b8b8eb463da0252dea2c53bbfcdc1
Joseph Brumaghim <joe@josephbrumaghim.com>
2016-01-05 13:01:37 -0500

    Remove plugins from default repo

--- server/plugins/SimpleChannels/simplechannels.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(chatter) {
-
-  this.onEnable = function() {
-    chatter.listenToAll("createChannel", function(data) {
-      chatter.createChannel(data.name);
-    })
-  }
-
-}
